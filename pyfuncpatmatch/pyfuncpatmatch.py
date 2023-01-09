48df3b7fc087ee5404eb0ea7882cae22bcd9a81b
Xavier Mitault <xavier.mitault@epitech.eu>
2023-01-09 22:40:02 +0100

    Add doc

--- pyfuncpatmatch/pyfuncpatmatch.py
+++ pyfuncpatmatch/pyfuncpatmatch.py
@@ -5,28 +5,101 @@ from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 
 class PatMatchAll:
+    """PatMatchAll.
+    If you want to match all
+
+    Useful for l_args:
+    ```python
+    @patfunc([1, PatMatchAll(), 2], ...
+    def abc(a, b, c):
+        ...
+    ```
+    """
+
     def __init__(self) -> None:
         pass
 
 
 class PatEqMatch:
+    """PatEqMatch.
+
+    ```python
+    @patfunc([PatEqMatch(1)], ...)
+    def abc(a):
+        ...
+    ```
+    will be the same as
+    ```python
+    @patfunc([1], ...)
+    def abc(a):
+        ...
+    ```
+    """
+
     def __init__(self, value: Any) -> None:
         self.value = value
 
 
 class PatEqMatchList(PatEqMatch):
+    """PatEqMatchList.
+
+    same as PatEqMatch but with list value (usefull for PatListExtract)
+    """
+
     def __init__(self, value: List[Any]) -> None:
         self.value = value
 
 
 class PatListExtract:
+    """PatListExtract.
+
+    Realy cool feature
+
+    Example:
+    ```python
+    @patfunc([""], {}, lambda x: print("Empty Name"))
+    @patfunc([PatListExtract()], {}, lambda x,y: print(x))
+    def print_initial(name: str):
+        ...
+    ```
+    will react like this:
+    ```python
+    >>> print_initial("")
+    Empty Name
+    >>> print_initial("a")
+    a
+    >>> print_initial("SupperName")
+    S
+    ```
+    Dont miss the first patfunc, else your code will raise an error for empty string
+    This apply for list too
+    """
+
     def __init__(
         self,
         var_name_fst: Optional[str] = None,
         var_name_rest: Optional[str] = None,
-        fst_eq_match: Union[PatEqMatch, PatMatchAll] = PatMatchAll(),
-        rest_eq_match: Union[PatEqMatchList, PatMatchAll] = PatMatchAll(),
+        fst_eq_match: Union[PatEqMatch, PatMatchAll, Any] = PatMatchAll(),
+        rest_eq_match: Union[PatEqMatchList, PatMatchAll, Any] = PatMatchAll(),
     ) -> None:
+        """__init__.
+
+        Parameters
+        ----------
+        var_name_fst : Optional[str]
+            name to give (kwargs) to the first elem, if None, it is passed as args, in place of the true arg order
+        var_name_rest : Optional[str]
+            name to give (kwargs) to the rest of the elem, if None, it is passed as args, after var_name_fst
+        fst_eq_match : Union[PatEqMatch, PatMatchAll, Any]
+            pattern match for the first elem
+        rest_eq_match : Union[PatEqMatchList, PatMatchAll, Any]
+            pattern match for the rest of the elem
+
+        Returns
+        -------
+        None
+
+        """
         self.var_name_fst = var_name_fst
         self.var_name_rest = var_name_rest
         self.fst_eq_match = fst_eq_match
@@ -165,6 +238,38 @@ def patfunc(
     k_args: Union[KArgsPatternMatch, PatMatchAll] = PatMatchAll(),
     func_instead: Optional[Callable] = None,
 ):
+    """patfunc.
+
+    Parameters
+    ----------
+    l_args : LArgsPatternMatch
+        list of pattern match / list extract to match for args
+    k_args : Union[KArgsPatternMatch, PatMatchAll]
+        dict of [keyword argument] = pattern match / list extract
+    func_instead : Optional[Callable]
+        function to call instead of the decorated function if patterns before are true
+
+    Examples
+    --------
+    ```python
+    @patfunc([0], {}, lambda _: 0)
+    @patfunc([1], {}, lambda _: 1)
+    def fib_rec(n: int):
+        return fib_rec(n - 1) + fib_rec(n - 2)
+    ```
+    will call the first lambda that return 0 if the first argument of fib_rec (n) is equal to 0
+    will call the first lambda that return 1 if the first argument of fib_rec (n) is equal to 1
+    else, it will call the decorated function
+    this is an equivalent (in a much beautiful way) to that:
+    ```python
+    def fib_rec(n: int):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        return fib_rec(n - 1) + fib_rec(n - 2)
+    ```
+    """
     if isinstance(l_args, list):
         l_args = tuple(l_args)
     if isinstance(k_args, PatMatchAll):
