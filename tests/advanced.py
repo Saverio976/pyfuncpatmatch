e4f0044f68f80c63e6758c3b155e5b23d67ab5ba
Xavier Mitault <xavier.mitault@epitech.eu>
2023-01-15 14:48:18 +0100

    Add tests for new features

--- tests/advanced.py
+++ tests/advanced.py
@@ -1,5 +1,6 @@
 from sys import exit
 from pyfuncpatmatch import patfunc, PatListExtract
+from pyfuncpatmatch.pat_match_types import PatEqMatch, PatGtMatch, PatLtMatch
 
 @patfunc([""], {}, lambda _: "Empty Name")
 @patfunc([PatListExtract(fst_match="s")], {}, lambda x,_: x)
@@ -30,4 +31,21 @@ if print_init("a") != "No":
     exit(1)
 print("print_ini 3")
 if print_init("as") != "a":
+    exit(1)
+
+
+@patfunc([PatLtMatch(3)], {}, lambda _: "LT")
+@patfunc([PatEqMatch(3)], {}, lambda _: "EQ")
+@patfunc([PatGtMatch(3)], {}, lambda _: "GT")
+def gt_than_3(_: int) -> str:
+    ...
+
+print("gt_than_3 1")
+if gt_than_3(0) != "LT":
+    exit(1)
+print("gt_than_3 2")
+if gt_than_3(3) != "EQ":
+    exit(1)
+print("gt_than_3 3")
+if gt_than_3(4) != "GT":
     exit(1)
